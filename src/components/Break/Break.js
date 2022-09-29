f6543ebd097b6e53dd4198c93067a73fb1fb5673
jibananando <jibananando.pramanik@gmail.com>
2022-09-29 22:25:43 +0600

    set local stroge

--- src/components/Break/Break.js
+++ src/components/Break/Break.js
@@ -3,7 +3,7 @@ import ExerciseDtails from '../ExerciseDtails/ExerciseDtails';
 import './Break.css'
 
 const Break = ({ timeData }) => {
-    const [breakTime,setBreakTime]=useState(null);
+    const [breakTime, setBreakTime] = useState(null);
     const breakTimeArry = [
         { id: 1, break: '10m' },
         { id: 2, break: '15m' },
@@ -11,16 +11,17 @@ const Break = ({ timeData }) => {
         { id: 4, break: '25m' },
         { id: 5, break: '30m' }
     ]
-    const handleBreak=(item)=>{
+    const handleBreak = (item) => {
         setBreakTime(item);
+        localStorage.setItem('Break', JSON.stringify(item));
     }
-    
-   
+
+
 
     return (
         <div>
             {
-                breakTimeArry.map(item => <span onClick={()=>handleBreak(item)} className='break-time' key={item.id}>{item.break}</span>)
+                breakTimeArry.map(item => <span onClick={() => handleBreak(item)} className='break-time' key={item.id}>{item.break}</span>)
             }
             <ExerciseDtails timeData={timeData} breakTime={breakTime}></ExerciseDtails>
         </div>
